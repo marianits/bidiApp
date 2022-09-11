46f7c8389f4a7fc5ddfbc9d4b3100639321c45ff
Mariana Arnez <mariana.arnez99@gmail.com>
2022-09-11 18:18:01 -0400

    Create client folder and install React dependencies

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import HomePage from "./pages/HomePage"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <HomePage />
+);
