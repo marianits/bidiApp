7d9e16e0116ab93533f3ea0789750f872dcdfe3e
Mariana Arnez <mariana.arnez99@gmail.com>
2022-09-26 17:17:52 -0400

    Add sidebar menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import { Routes, Route } from 'react-router-dom'
-import Homepage from './pages/homePage'
+import Homepage from './pages/HomePage'
 import Navbar from './components/navbar'
 import Register from './pages/register'
 import Login from './pages/login'
+import Autores from './pages/Autores'
+import Categorias from './pages/Categorias'
 
 export default function App() {
     return (
@@ -12,6 +14,8 @@ export default function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/registro" element={<Register />} />
+          <Route path="/categorias" element={<Categorias />} />
+          <Route path="/autores" element={<Autores />} />
         </Routes>
       </>
     )
